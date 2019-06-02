d90386b19cd6cb44ead308cfe62ef0010debff34
Yury Skaletskiy <yurys@s-techlab.ru>
2019-06-02 13:14:41 +0300

    added .idea + template fixz

--- three-templates/template_3.py
+++ three-templates/template_3.py
@@ -30,12 +30,68 @@ def get_model_3():
     }
 
 
+# SQL CASE Anti-pattern
+class SqlAntipattern(object):
+    def render(self, m):
+        if m["typeList"]:
+            return self.render_typelist(m)
+        if m["typeCountGroupBy"]:
+            return self.render_groupby(m)
+
+        raise Exception("Unknown query type")
+
+    def render_where(self, m):
+        sql = "WHERE "
+
+        def eqtion(w):
+            return "{}={}{}{}".format(
+                w["field"],
+                "'" if "quot" in w else "",
+                w["eq_val"],
+                "'" if "quot" in w else "")
+
+        wheres = [eqtion(w) for w in m["where"]]
+        sql += ", ".join(wheres)
+        return sql + " "
+
+    def render_orderby(self, m):
+        return "ORDER BY " + m["orderBy"]
+
+    def render_typelist(self, m):
+        sql = "SELECT * FROM " + m["tableName"] + " "
+        if "hasWhere" in m:
+            sql += self.render_where(m)
+
+        if "orderBy" in m:
+            sql += self.render_orderby(m)
+
+        if "limit" in m:
+            sql += " LIMIT " + m["limit"]
+
+        return sql
+
+    def render_groupby(self, m):
+        sql = "SELECT " + m["groupColumn"]
+        sql += ", COUNT(*) FROM " + m["tableName"] + " "
+        sql += " GROUP BY " + m["groupColumn"]
+
+        return sql
+
+
 model = get_model_1()
 with open(os.path.join("templates", "query.sql.mustache"), 'r') as f:
     template = f.read()
 
 sql = pystache.render(template, model)
 print(sql)
+
+
+print("-- CODE-BASED RENDERING")
+sql = SqlAntipattern().render(model)
+print(sql)
+print("-- CODE-BASED RENDERING END")
+
+
 
 model = get_model_2()
 with open(os.path.join("templates", "query.sql.mustache"), 'r') as f:
